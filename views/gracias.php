7fcf2ad2686722318d4e501e2f6dcf6c7f1bd5ea
unknown <florencia.fernandezb@davinci.edu.ar>
2022-10-02 19:05:58 -0300

    process form#

--- views/gracias.php
+++ views/gracias.php
@@ -1,7 +1,13 @@
+<?php
+
+$name = $_POST['nombre'];
+$comment = $_POST['comentarios'];
+
+?>
 <main>
     <section class="gracias">
-        <h2>¡Gracias por contactarnos!</h2>
-        <p>Tu mensaje ha sido enviado. Te responderemos a la brevedad.</p>
+        <h2>¡Gracias  <?= $name ?> por contactarnos!</h2>
+        <p>Tu mensaje <q> <?= $comment ?>!</q> ha sido enviado. Te responderemos a la brevedad.</p>
         <a href="index.php">Seguir navegando</a>
     </section>
 </main>
