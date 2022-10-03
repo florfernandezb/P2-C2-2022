b6eefdc6b709089f4a17dc05174482d21d309eb5
unknown <florencia.fernandezb@davinci.edu.ar>
2022-10-02 23:21:09 -0300

    improve home

--- views/home.php
+++ views/home.php
@@ -4,12 +4,12 @@
         <div class="about">
             <p>¿Alguna vez quisieron tener una marca propia o su propio emprendimiento? ¡Yo sí! 
                 Soñé mucho tiempo con tener mi marca de vestidos, pero nunca le puse lo suficiente de mí para lograrlo. Siempre ponía alguna excusa para empezarlo. Más allá de eso, quería tener algo propio, 100% hecho por mí. 
-                Un día revolviendo instagram me encontré con @casarriba el taller de @guiceramicas. Con Gui aprendí moldearía -un viaje de ida-. Buscar qué pieza hacer, intercambiar moldes con las chicas del taller, esmaltar y esperar a ver las piezas que salen del horno se convirtió en un momento mágico: mi preferido.
+                Un día revolviendo instagram me encontré con @casarriba el taller de @guiceramicas. Con Gui aprendí moldearía -un viaje de ida-. <em>Buscar qué pieza hacer, intercambiar moldes con las chicas del taller, esmaltar y esperar a ver las piezas que salen del horno se convirtió en un momento mágico: mi preferido.</em>
                 La pandemia y los tropiezos me demostraron que esto puede no ser solo un pasatiempo sino mi nuevo trabajo porque de golpe y sin buscarlo, con muchos miedos y desafíos, me encontré con este emprendimiento propio. Sin duda alguna, proyecto al que le pongo todo mi amor y mi cuerpo, al que decidí llamarlo @hechopor.vicki.
             </p>
             <p>Gracias a todos por acompañarme.</p>
-            <p>Hecho por Vicki</p>
-            <p>A mano en Argentina</p>
+            <p><strong>Hecho por Vicki</strong></p>
+            <p><em>A mano en Argentina</em></p>
         </div>
 
         
