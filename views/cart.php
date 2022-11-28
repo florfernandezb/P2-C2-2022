62fea882676d32a4b3abfed29890431b2d6e3edd
Manuela Jaureguialzo <72824904+manuelajzo@users.noreply.github.com>
2022-11-27 22:08:47 -0300

    carrito style

--- views/cart.php
+++ views/cart.php
@@ -9,33 +9,38 @@ $items = $miCarrito->get_carrito();
 
     <?PHP if (count($items)) { ?>
         <form action="actions/update_items_acc.php" method="POST">
-            <table class="table">
+        <div class="row justify-content-center">
 
+       
+        <div class="col-auto table">
+            <table class="table text-center">
                 <thead>
                     <tr>
-                        <th scope="col" width="15%">Producto</th>
-                        <th scope="col" width="15%">Cantidad</th>
-                        <th class="text-end" scope=" col" width="15%">Precio</th>
-                        <th class="text-end" scope="col" width="10%"></th>
+                        <th scope="col" >Producto</th>
+                        <th scope="col" >Cantidad</th>
+                        <th class="text-end" scope="col" >Precio</th>
                     </tr>
                 </thead>
                 <tbody>
                     <?PHP foreach ($items as $key => $item) { ?>
                         <tr>
-                            <td class="align-middle">
+                            <td class="align-middle text-center cartProd">
+                                <img src="<?= './res/products/'. $item['img'] . '.png';?>" alt="Imagen Ilustrativa de <?= $item['product'] ?>" class="img-fluid rounded">
                                 <h2 class="h5"><?= $item['product'] ?></h2>
-                                <img src="<?= './res/products/'. $item['img'] . '.png';?>" alt="Imágen Illustrativa de <?= $item['product'] ?>" class="img-fluid rounded shadow-sm">
-                                <p><?= $item['product'] ?></p>
                             </td>
-                            <td class="align-middle">
-                                <label for="q_<?= $key ?>" class="visually-hidden">Cantidad</label>
-                                <input type="number" class="form-control" value="<?= $item['cantidad'] ?>" id="q_<?= $key ?>" name="q[<?= $key ?>]">
-                            </td>
-                            <td class="text-end align-middle">
-                                <p class="h5 py-3">$<?= number_format($item['cantidad'] * $item['precio'], 2, ",", ".") ?></p>
-                            </td>
-                            <td class="text-end align-middle">
-                                <a href="actions/remove_item_acc.php?id=<?= $key ?>" class="btn btn-sm btn-danger">Eliminar</a>
+                            <td class="align-middle text-center cartCant">
+                                    <div>
+                                    <label for="q_<?= $key ?>" class="visually-hidden">Cantidad</label>
+                                    <input type="number" class="form-control" value="<?= $item['cantidad'] ?>" id="q_<?= $key ?>" name="q[<?= $key ?>]">
+                                    <input type="submit" value="Actualizar Cantidad" class="btn btnActCant">
+                                </div>
+                                </td>
+                                <td class="text-end align-middle text-center cartPrice">
+                                    <p class="h5 py-3">$<?= number_format($item['cantidad'] * $item['precio'], 2, ",", ".") ?></p>
+                                </td>
+                            </div>
+                            <td class="text-end align-middle text-center divCartDel">
+                            <a href="actions/remove_product_acc.php?id=<?= $key ?>" class="delete" data-toggle="modal"><i class="bi bi-trash-fill" data-toggle="tooltip" title="" data-original-title="Delete"></i></a>
                             </td>
                         </tr>
                     <?PHP } ?>
@@ -54,14 +59,15 @@ $items = $miCarrito->get_carrito();
 
 
             </table>
-
+            </div>
+            </div>
 
 
             <div class="d-flex justify-content-end gap-2">
-                <input type="submit" value="Actualizar Cantidades" class="btn btn-warning">
-                <a href="index.php?s=productList" role="button" class=" btn btn-danger">Seguir comprando</a>
-                <a href="actions/clear_cart_acc.php" role="button" class="btn btn-danger">Vaciar Carrito</a>
-                <a href="#" role="button" class="btn btn-primary">Finalizar Compra</a>
+                
+                <a href="index.php?s=productList" role="button" class=" btn btnSeguirComprando"><i class="bi bi-arrow-left biIcons"></i>Seguir comprando</a>
+                <a href="actions/clear_cart_acc.php" role="button" class="btn btnVaciar"><i class="bi bi-trash-fill biIcons" data-toggle="tooltip" title="" data-original-title="Delete"></i>Vaciar Carrito</a>
+                <a href="#" role="button" class="btn btnFinalizar">Finalizar Compra<i class="bi bi-arrow-right biIcons"></i></a>
             </div>
 
         </form>
